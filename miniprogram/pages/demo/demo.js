4afdc9614ca87926ad49e13f8ff056f951c5244d
ss <s22539634@aliyun.com>
2019-03-19 08:55:43 +0800

    111

--- miniprogram/pages/demo/demo.js
+++ miniprogram/pages/demo/demo.js
@@ -1,5 +1,11 @@
+import util from '../../public/util';
 Page({
   data:{
 
+  },
+  onLoad:function () {
+    var time = 1552878152000;
+    console.log(util.stringTime(1552878152000/1000,'D'));
+    console.log(util.beforeTime(time/1000));
   }
 })
---
+++ miniprogram/public/util.js
@@ -0,0 +1,48 @@
+module.exports = {
+  stringTime:stringTime,
+  beforeTime:beforeTime,
+}
+function stringTime(time,type) {
+  //time为传入的时间戳 ，type为传入希望返回的值
+  // 这里定义：已传入的值为截至日期
+  // Y返回年
+  // M返回年月
+  // D返回年月日
+  // h返回年月日时
+  // m返回年月日时分
+  // s返回年月日时分秒
+  var len;
+  if (time == null) return 0;
+  if (typeof time != "string") time += "";
+  len = time.replace(/[^x00-xff]/g,"01").length;
+  //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  var date = new Date(time*1000);
+  // len <= 10 ? date.setDate(time) : date.setDate(time*1000);
+  var Y = date.getFullYear();
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+  var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate());
+  var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours());
+  var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+  var s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  return 'Y' == type ? Y :
+          'M' == type ?  Y + '-' + M :
+            'D' == type ? Y + '-' + M + '-' + D :
+              'h' == type ? Y + '-' + M + '-' + D + ' ' + h :
+                'm' == type ? Y + '-' + M + '-' + D + ' ' + h + ':' + m :
+                  Y + '-' + M + '-' + D + ' ' + h + ':' + m + ':' + s;
+}
+// 时间处理，传入时间戳
+function beforeTime(timestamp){
+  var mistiming = Math.round(new Date() / 1000)-timestamp;
+  var postfix = mistiming>0 ? '前' : '后';
+  mistiming = Math.abs(mistiming)
+  var arrr = ['年','个月','星期','天','小时','分钟','秒'];
+  var arrn = [31536000,2592000,604800,86400,3600,60,1];
+
+  for(var i=0; i<7; i++){
+    var inm = Math.floor(mistiming/arrn[i])
+    if(inm!=0){
+      return inm+arrr[i] + postfix
+    }
+  }
+}
