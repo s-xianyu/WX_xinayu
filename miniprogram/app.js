e98dc691e0cf3512c892f8115e2f90f388bca4e5
ss <s22539634@aliyun.com>
2019-04-10 16:30:43 +0800

    1

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,30 +1,78 @@
 //app.js
-let that;
+let vm;
 App({
-  G:{
-    host:'https://www.xianyu.com',
+  G: {
+    host: 'https://www.xianyu.com',
     INFO: {}, //保存用户登录信息
-    IPX:false, //全局添加iPhoneX判断
-    WIN:'', //保存系统信息
+    code: '',
+    IPX: false, //全局添加iPhoneX判断
+    WIN: '', //保存系统信息
   },
-  onLaunch: function() {
-    that = this;
-    console.log(that.G)
+  onLaunch: function () {
+    vm = this;
 
     wx.getSystemInfo({
       success: res => {
-        that.G.WIN = res;
-        let model = res.model.substring(0,res.model.indexOf('X'))+'X' //获取手机信息是否为iPhone X
-        if(model == 'iPhone X'){
-          that.G.IPX = true;
+        vm.G.WIN = res;
+        //获取手机信息是否为iPhone X
+        let model = res.model.substring(0, res.model.indexOf('X')) + 'X'
+        if (model == 'iPhone X') {
+          vm.G.IPX = true;
         }
       }
-    })
+    });
   },
-  onShow:function () {
-    let info = wx.getStorageSync('INFO');
-    if(info != ''){
-      debugger
+  onShow: function () {
+    //获取code
+    wx.login({
+      success: res => {
+        vm.G.INFO = res.code;
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    });
+    //获取storage ，如果存在，获取登录信息
+    debugger
+    let userInfo = wx.getStorageSync('INFO');
+    if (userInfo != '') {
+      wx.request({
+        url: vm.G.host + 'mobile/userInfo.json',
+        data: {
+          mobile: userInfo.mobile,
+          token: userInfo.token,
+        },
+        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        header: {
+          'from': 'miniprogram'
+        }, // 设置请求的 header
+        success: res => {
+          if (res.data.message == '请先登录') {
+            wx.removeStorageSync('INFO');
+            vm.G.INFO = {};
+            vm.onLaunch();
+          } else if (res.data.message == 'success') {
+            let loginInfo = {
+              mobile: res.mobile,
+              token: res.token,
+              phone: res.phone,
+            };
+            //保存登录信息
+            wx.setStorageSync('INFO', loginInfo);
+          }
+        },
+        fail: function () {
+          // fail
+        },
+        complete: function () {
+          // complete
+        }
+      })
+    } else {
+      console.log('未登录状态')
     }
   }
 })
--- miniprogram/pages/demo/demo.js
+++ miniprogram/pages/demo/demo.js
@@ -1,15 +1,23 @@
 import util from '../../public/util';
 import common from '../../public/util';
+let vm;
 Page({
   data:{
     userInfo:'',
   },
   onLoad:function () {
+    vm = this;
     var time = 1552878152000;
     console.log(util.stringTime(1552878152000/1000,'D'));
     console.log(util.beforeTime(time/1000));
   },
   ok:()=>{
     console.log('成功')
+  },
+  onGetUserInfo:(e)=>{
+    console.log(e.detail.userInfo)
+    vm.setData({
+      userInfo:e.detail.userInfo
+    })
   }
 })
